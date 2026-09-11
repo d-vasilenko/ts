c5caa266e7151b80dbb3c47a86f50c6a3ed30ed7
Denis <ghostinchip@gmail.com>
2026-09-11 21:29:49 +0300

    wip:

---
+++ ts-pure/02-basic-types/01-primitive-types.ts
@@ -0,0 +1,17 @@
+
+const age: number = 40;
+const price: number = 10.40;
+
+const firstName: string = 'Alice';
+const helloMessage: string = `Hello ${firstName}`;
+
+console.log(helloMessage);
+
+const isAvalible: boolean = true;
+const hasPermissions: boolean = false;
+
+const emptyNull: null = null;
+
+const notDefined: undefined = undefined;
+
+console.log(isAvalible, hasPermissions, emptyNull, notDefined);
---
+++ ts-pure/02-basic-types/02-arrays-tuples.ts
@@ -0,0 +1,5 @@
+const numbers: number[] = [1, 3, 4, 5];
+const names: string[] = ['Alice', 'Alex', 'Mil'];
+
+const personTuple: [string, number] = ['Alex', 30];
+const some: (string | number)[] = [49, 'Denis', 29, 'Masivo'];
