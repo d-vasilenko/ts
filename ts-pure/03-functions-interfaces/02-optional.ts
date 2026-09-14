57acd203103d3529f396b0d31720970877af7337
Denis <ghostinchip@gmail.com>
2026-09-14 21:13:47 +0300

    wip:

---
+++ ts-pure/03-functions-interfaces/02-optional.ts
@@ -0,0 +1,9 @@
+function geetHell(name: string, message?: string): string {
+  if (message) {
+    return `${name}, ${message}`;
+  }
+  return `Hello, ${name}`;
+}
+
+console.log('Jhack');
+console.log('Jack', 'How are you');
